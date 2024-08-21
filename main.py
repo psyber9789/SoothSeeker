8d4f8674a367a91b8fd9ebdfe7a8d68faa0d9f72
C. X. Stewart <majestic-12@outlook.com>
2024-08-21 06:48:59 +0100

    update file docstring for main.py

--- main.py
+++ main.py
@@ -1,4 +1,7 @@
 """
+Work in progress. For the next while this file will contain top-level code for executing code during
+the initial development, before this file is then delegated solely to instantiating the application
+instance, whereby an applicaton controller will initialise the application.
 """
 
 import sys
